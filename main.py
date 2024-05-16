216fc77a461936283c96a1c2662397d41ead9b3e
Артем Малиш <malyshartem@gmail.com>
2024-05-16 16:38:03 +0300

    good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def print_hi(name):
-    print(f'Hi, {name}') # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Привіт, {name}')
+    print(f'Раді вітати!!!')
 
 if __name__ == '__main__':
-    print_hi('PyCharm!')
+    print_hi(input("Введіть Ваше ім'я, будь ласка: "))
